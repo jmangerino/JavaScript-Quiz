c65115201249377f19f9fd3100c4d54da234550e
Josh Man <joshuamangerino@gmail.com>
2022-05-15 12:38:11 -0600

    added questions to JavaScript

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+// questions to be asked and the answers stored in an array
+var questions = [
+    {
+        questiontext: "Commonly used data types Do Not incluede:",
+        answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers",],
+        correctanswer: "3. alerts",
+    },
+    
+    {
+        questiontext: "The condtion in an if/else statement is encloded with ___.",
+        answers: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets",],
+        correctanswer: "3. parenthesis"
+    },
+
+    {
+        questiontext: "Arrays in JavaScript can be used to store ____.",
+        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above",],
+        correctanswer: "4. all of the above",
+    },
+
+    {
+        questiontext: "String values must be enclosed within ___ when being assigned to variables",
+        answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses",],
+        correctanswer: "3. quotes",
+    }
+
+    {
+        questiontext: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log",],
+        correctanswer: "4. console.log",
+    },
+]
+
+
+
