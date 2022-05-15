03d561853ac4ea64957a09fcfff7dfa5d1c79b7b
Josh Man <joshuamangerino@gmail.com>
2022-05-15 14:53:05 -0600

    finished quiz portion of javascript

--- assets/script.js
+++ assets/script.js
@@ -22,7 +22,7 @@ var questions = [
         questiontext: "String values must be enclosed within ___ when being assigned to variables",
         answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses",],
         correctanswer: "3. quotes",
-    }
+    },
 
     {
         questiontext: "A very useful tool used during development and debugging for printing content to the debugger is:",
@@ -31,5 +31,115 @@ var questions = [
     },
 ]
 
+// this is all the variables we will need
+var maincard = document.getElementById("main-card");
+var quizcard = document.getElementById("quiz-card");
+var scorecard = document.getElementById("score-card");
+var highscorecard = document.getElementById("highscore-card");
+var results = document.getElementById("results");
+var resultstext = document.getElementById("results-text");
+var interval;
+var time;
+var questionIndex;
+
+
+// this function is to hide the cards till we need them
+function hidecards() {
+    maincard.setAttribute("hidden", true);
+    quizcard.setAttribute("hidden", true);
+    scorecard.setAttribute("hidden", true);
+    highscorecard.setAttribute("hidden", true);
+}
+
+function hideresultstext() {
+    results.style.display= "none";
+}
+
+document.getElementById("start-button").addEventListener("click", startquiz);
+
+function startquiz() {
+    hidecards();
+    quizcard.removeAttribute("hidden");
+    questionIndex = 0
+    displayQuestion();
+    time = questions.length * 10;
+    interval = setInterval(countdown, 1000);
+    displayTime();
+}
+
+function countdown() {
+    time--;
+    displayTime() 
+        timeDisplay.textContent = time;
+        if (time < 1) {
+            endQuiz();
+        }
+    }
+
+var timeDisplay = document.getElementById("time");
+
+function displayTime() {
+    timeDisplay.textContent = time;
+}
+
+function displayQuestion() {
+    let question = questions[questionIndex];
+    let answers = question.answers;
+
+    let h2QuestionElement = document.getElementById("question-text");
+    h2QuestionElement.textContent = question.questiontext;
+
+    for (let i = 0; i < answers.length; i++) {
+        let answer = answers[i];
+        let answerButton = document.querySelector("#answer" + i);
+        answerButton.textContent = answer
+    }
+}
+
+document.getElementById("quiz-answers").addEventListener("click", checkcorrectAnswer);
+
+function answerIsCorrect(answerButton) {
+    return answerButton.textContent === questions[questionIndex].correctanswer
+}
+
+function checkcorrectAnswer(eventObject) {
+    let answerButton = eventObject.target;
+    results.style.display = "block";
+    if (answerIsCorrect(answerButton)) {
+        resultstext.textContent = "Correct!";
+        setTimeout(hideresultstext, 1000);
+    } 
+    else {
+        resultstext.textContent = "Incorrect!";
+        setTimeout(hideresultstext, 1000);
+        if (time >= 5) {
+            time = time - 5;
+            displayTime();
+        }
+        else {
+            time = 0;
+            displayTime ()
+            endQuiz();
+        }
+    }
+
+    questionIndex++;
+    if (questionIndex < questions.length) {
+        displayQuestion();
+    }
+    else {
+        endQuiz();
+    }
+}
+
+var score = document.getElementById("score");
+
+function endQuiz() {
+    clearInterval(interval);
+    hidecards();
+    highscorecard.removeAttribute("hidden");
+    score.textContent = time;
+    console.log(score)
+}
 
 
